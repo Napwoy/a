30aa67a807e2d9ea805f7426041cba4196ba071c
Nabil Aditia Putra <106542613+Napwoy@users.noreply.github.com>
2026-06-01 21:25:19 +0700

    Delete 1_5053360887277028275 directory

--- 1_5053360887277028275/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractProvider } from 'web3-core'
-import Web3 from '~/node_modules/web3/types'
-declare global {
-  interface Window {
-    web3?: Web3
-    ethereum?: AbstractProvider
-  }
-}
